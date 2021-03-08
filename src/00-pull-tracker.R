fc8cd8616794d9ab4577a197bb58a6aacb6b106b
Michael DeWitt <michael.dewitt@conehealth.com>
2021-03-08 11:38:53 -0500

    fix to try multiple address

--- src/00-pull-tracker.R
+++ src/00-pull-tracker.R
@@ -9,7 +9,7 @@ calendar_use <- seq.Date(from = as.Date("2021-02-28"),
                          by = "day")
 calendar_use = calendar_use[lubridate::wday(calendar_use, label = TRUE) %in% c("Sun", "Tue", "Thu")]
 
-nearest_date = calendar_use[calendar_use>=Sys.Date()][1]
+nearest_date = calendar_use[calendar_use>=Sys.Date()-1][1]
 
 date_for_cdc = format(nearest_date, "%m%d%Y")
 date_for_cdc_alt = format(nearest_date, "%m%d%y")
@@ -19,19 +19,27 @@ url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/transmission/docs/%s_W
 dat_in = tryCatch(data.table::fread(url),
                   error = function(e) NULL)
 
-if(!is.null(dat_in)){
+if(is.null(dat_in)){
   url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/transmission/docs/%s-Web-UpdateCSV-TABLE.csv", date_for_cdc_alt)
 
   dat_in = tryCatch(data.table::fread(url),
                     error = function(e) NULL)
 }
 
-if(!is.null(dat_in)){
+if(is.null(dat_in)){
   url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/transmission/docs/%s_Web-UpdateCSV-TABLE.csv", date_for_cdc_alt)
 
   dat_in = tryCatch(data.table::fread(url),
                     error = function(e) NULL)
 }
+
+if(is.null(dat_in)){
+  url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s-Web-UpdateCSV-TABLE.csv", date_for_cdc_alt)
+
+  dat_in = tryCatch(data.table::fread(url),
+                    error = function(e) NULL)
+}
+
 
 if(!is.null(dat_in)){
   data.table::fwrite(dat_in, file.path("data-raw", basename(url)))
