897f9937027c6a2d62a1d89e904989717f4a54ce
Michael DeWitt <michael.dewitt@conehealth.com>
2021-03-23 09:21:55 -0400

    updating for vriants

--- src/03-combine-nc.R
+++ src/03-combine-nc.R
@@ -1,6 +1,6 @@
 # Purpose: Aggregate Data
 library(data.table)
-
+library(ggplot2)
 
 # target files ------------------------------------------------------------
 csv_to_combine = list.files(path = "data-raw", full.names = TRUE, pattern = "*.csv")
@@ -18,3 +18,17 @@ dat_raw = rbindlist(dat_raw, idcol = "update_date")
 dat_raw[,update_date:=as.Date(update_date)]
 
 nc_only = dat_raw[State=="NC"]
+
+nc_long = melt(nc_only, id.vars = c("State", "update_date"))
+
+ggplot(data = nc_long, aes(update_date, value, colour = variable))+
+  geom_line(size = 1.2)+
+  theme_bw()+
+  scale_colour_manual(values = c("#00468BFF", "#ED0000FF", "#42B540FF"))+
+  labs(
+    title = "Reported SARS-CoV-2 Variants",
+    subtitle = "For North Carolina",
+    caption = "Data from Centers for Disease Control",
+    colour = "Variants",
+    y = "Count of Identified Samples"
+  )+ggsave(filename = "output/nc-variants-cdc.pdf")
