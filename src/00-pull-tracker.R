dd64d4b41c590380625b0dc3af004b3c78512267
Michael DeWitt <michael.dewitt@conehealth.com>
2021-04-07 12:14:31 -0400

    try a ton of combination given haphazard file positing

--- src/00-pull-tracker.R
+++ src/00-pull-tracker.R
@@ -16,48 +16,32 @@ date_for_cdc_alt = format(nearest_date, "%m%d%y")
 "https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/03302021_Web-Update.csv"
 url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/transmission/docs/%s_Web-UpdateCSV-TABLE.csv", date_for_cdc)
 
-dat_in = tryCatch(data.table::fread(url),
-                  error = function(e) NULL)
+date_possibilities<- expand.grid(date = c(date_for_cdc, date_for_cdc_alt),
+                       extensions = c("xlsx", "xls", "csv"))
 
-if(is.null(dat_in)){
-  url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/transmission/docs/%s-Web-UpdateCSV-TABLE.csv", date_for_cdc_alt)
+url_combinations <- c(
+sprintf("https://www.cdc.gov/coronavirus/2019-ncov/transmission/docs/%s-Web-UpdateCSV-TABLE.%s",date_possibilities$date, date_possibilities$extensions ),
+sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s_Web-Update.%s",date_possibilities$date, date_possibilities$extensions),
+sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s_Web-UpdateCSV-TABLE.%s",date_possibilities$date, date_possibilities$extensions),
+sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s-Web-UpdateCSV-TABLE.%s",date_possibilities$date, date_possibilities$extensions )
+)
 
-  dat_in = tryCatch(data.table::fread(url),
-                    error = function(e) NULL)
-}
-
-if(is.null(dat_in)){
-  url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s_Web-Update.csv", date_for_cdc)
-
-  dat_in = tryCatch(data.table::fread(url),
-                    error = function(e) NULL)
-}
-
-if(is.null(dat_in)){
-  url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/transmission/docs/%s_Web-UpdateCSV-TABLE.csv", date_for_cdc)
-
-  dat_in = tryCatch(data.table::fread(url),
-                    error = function(e) NULL)
-}
-
-if(is.null(dat_in)){
-  url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s-Web-UpdateCSV-TABLE.csv", date_for_cdc_alt)
-
-  dat_in = tryCatch(data.table::fread(url),
-                    error = function(e) NULL)
-}
-
-if(is.null(dat_in)){
-  url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s_Web-UpdateCSV-TABLE.csv", date_for_cdc)
-  dat_in = tryCatch(data.table::fread(url),
-                    error = function(e) NULL)
-}
-
-if(is.null(dat_in)){
-  url <- sprintf("https://www.cdc.gov/coronavirus/2019-ncov/downloads/transmission/%s-Web-UpdateCSV-TABLE.csv", date_for_cdc)
-  dat_in = tryCatch(data.table::fread(url),
-                    error = function(e) NULL)
-}
+dat_in = NULL
+i <- 1
+while(is.null(dat_in)){
+  tmp <- tempfile(fileext = ".csv")
+  import = tryCatch(download.file(url_combinations[i],destfile = tmp),
+           error = function(e) NULL)
+  if(!is.null(import)){
+    dat_in = data.table::fread(tmp)
+  } else {
+    dat_in = NULL
+  }
+  i = i+1
+  if(i > length(url_combinations)){
+    break()
+  }
+  }
 
 
 if(!is.null(dat_in)){
