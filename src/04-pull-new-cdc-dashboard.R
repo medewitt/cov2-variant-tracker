c5b2e8324f8b1bfb5446d2f5a252fbc31a296060
Michael DeWitt <me.dewitt.jr@gmail.com>
2021-10-21 09:32:26 -0400

    updated code to reflect new data source and format

--- src/04-pull-new-cdc-dashboard.R
+++ src/04-pull-new-cdc-dashboard.R
@@ -1,7 +1,11 @@
 # Purpose: Pull down new CDC Dashboard for Variant Tracking
 
 url <- "https://public.tableau.com/views/State_Proportions_table/StateProportionsDash.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0"
+
+
+url <- "https://public.tableau.com/views/WeightedStateVariantTable/StateVBMTable.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0"
+
 current_time <- format(Sys.time(), "%Y%m%d%H%M")
-try(download.file(url = url, destfile = here::here("data-raw","new-format",
+try(download.file(url = url, destfile = here::here("data-raw","format-202110",
                                                    paste0(current_time,"_cdc_metrics.csv"))))
 
--- src/05-clean-cdc-data-new.R
+++ src/05-clean-cdc-data-new.R
@@ -35,6 +35,39 @@ dat_nc_sequence <- dat_nc[grepl("Total", `Measure Names`)][,NewSequenceCT:=c(0,d
 
 dat_ratio <- merge(dat_ratio, dat_sequences[,.(UpdateDTS,State,samples)], by = c("UpdateDTS", "State"))
 
+
+
+# new format --------------------------------------------------------------
+# Added as of October 20, 2021 due to changes in how the data are presented.
+
+docs <- fs::dir_ls(here::here("data-raw", "format-202110"))
+dat <- lapply(docs, fread)
+names(dat) <- stringr::str_extract(basename(docs), "\\d+")
+
+dat_raw <- rbindlist(dat, idcol = "UpdateDTS")
+
+dat_raw[,UpdateDTS := as.POSIXct(UpdateDTS,format = "%Y%m%d%H%M")]
+
+dat_raw[,`Measure Values`:=stringr::str_remove(`Measure Values`, ",")]
+
+dat_raw[,`Measure Values`:=as.numeric(`Measure Values`)]
+
+dat_raw[,`Total Sequences`:=stringr::str_remove(`Total Sequences`, ",")]
+
+dat_raw[,`Total Sequences`:=as.numeric(`Total Sequences`)]
+
+dat_raw <- dat_raw[`Measure Names`=="Share"]
+
+str(dat_ratio)
+
+dat_raw[,`Measure Names`:=NULL]
+
+str(dat_raw)
+
+names(dat_raw) <- c("UpdateDTS", "State", "Measure Names", "Measure Values", "samples")
+
+combined_out <- rbind(dat_ratio,dat_raw)
+
 # dat_nc_ratio %>%
 #   ggplot(aes(UpdateDTS,`Measure Values`, colour = `Measure Names`))+
 #   geom_line()+
